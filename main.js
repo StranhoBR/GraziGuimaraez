ac6039f33132ef9cb9cd83fb8f87bdb0bcfe04c9
Anderson <stranhol2@gmail.com>
2022-04-30 13:45:14 -0300

    Quarto Commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// TIPOS DE DADOS DO JS
+
+// char = string 'um texto aqui'
+
+// number 12345
+
+//bollean = true or false
+
+////////////////////////////////////////////////////////////////////////
+
+// DOM  - DOCUMENT OBJECT MODEL
+
+/* ABRE E FECHA O MENU QUANDO CLICAR NO ICONE - HAMBURGUE E X */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* QUANDO CLICAR EM UM ITEM DO MENU ESCONDER O MENU */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
