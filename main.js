244affe394017a4dc545befbc524c17594f1d148
Anderson <stranhol2@gmail.com>
2022-05-01 22:20:08 -0300

    Decimo Commit

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ window.addEventListener('scroll', function () {
   }
 })
 
-/* Testimonials carroseul slider swiper */
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -40,3 +40,21 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
